f5197c9533a84aff43eb68c76f5d544c78170f50
= <Emmanuelsumeh@gmail.com>
2022-09-09 14:57:53 +0100

    feat: smart contract interaction

---
+++ helpers/nft/index.js
@@ -0,0 +1,18 @@
+import {Web3Storage} from 'web3.storage/dist/bundle.esm.min.js'
+
+// function to upload a file to IPFS via web3.storage
+export const uploadFileToWebStorage = async (e) => {
+    // Construct with token and endpoint
+    const client = new Web3Storage({token: process.env.REACT_APP_STORAGE_API_KEY})
+
+    const file = e.target.files;
+    if (!file) return;
+    // Pack files into a CAR and send to web3.storage
+    const rootCid = await client.put(file) // Promise<CIDString>
+
+    // Fetch and verify files from web3.storage
+    const res = await client.get(rootCid) // Promise<Web3Response | null>
+    const files = await res.files() // Promise<Web3File[]>
+
+    return `https://ipfs.infura.io/ipfs/${files[0].cid}`;
+}
--- pages/counter/index.js
+++ pages/counter/index.js
@@ -4,22 +4,24 @@ import {useContractWrite, usePrepareContractWrite} from "wagmi";
 import {contractAddress} from "../../helpers/constants";
 import CounterAbi from "../../contracts/abi/counter.json";
 import {useContractSend} from "../../hooks/contract/useContractWrite";
+import Spinner from "../../components/Spinner";
 
 const Counter = () => {
     const {data, isLoading} = useContractCall("get");
 
-    const {write : incrementCounter} = useContractSend('inc')
-    const {write : decrementCounter} = useContractSend('dec')
+    const {write: incrementCounter, isSuccess : incSuccess, isLoading: incLoading} = useContractSend('inc')
+    const {write: decrementCounter, isLoading: decLoading} = useContractSend('dec')
 
-    const increment = () => {
-        incrementCounter()
+    const increment = async () => {
+        await incrementCounter()
     }
     const decrement = () => {
         decrementCounter()
     }
     return (
-        <div >
-            <main className="min-h-full flex-col text-center justify-center border border-black border-2 mt-20 max-w-lg mx-auto sm:px-6 lg:px-8">
+        <div>
+            <main
+                className="min-h-full flex-col text-center justify-center border border-black border-2 mt-20 max-w-lg mx-auto sm:px-6 lg:px-8">
                 <div>
                     <p className={"text-3xl mt-3"}> Counter </p>
                 </div>
@@ -40,7 +42,9 @@ const Counter = () => {
                         type="button"
                         className="inline-flex items-center  px-8 py-4 border border-transparent text-xl leading-4 font-medium rounded-md shadow-sm text-dark border border-2 border-black bg-white hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
-                    Decrease
+                        {decLoading ?
+                            <Spinner color={"black"} loading={true}/> : "Decrease"
+                        }
                     </button>
 
                     <button
@@ -49,7 +53,9 @@ const Counter = () => {
                         type="button"
                         className="inline-flex items-center px-8 py-4 border border-transparent text-xl leading-4 font-medium rounded-md shadow-sm text-white bg-black hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
-                        Increase
+                        {incLoading ?
+                            <Spinner loading={true}/> : "Increase"
+                        }
                     </button>
                 </div>
 
