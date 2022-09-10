3150372a881d51b2c9986f6b23e9f9188d60886d
moritzfelipe <stellmacher@gmail.com>
2022-09-10 16:41:23 +0200

    Refactor search

---
+++ components/SearchBar/index.js
@@ -0,0 +1,46 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from 'react';
+
+const SearchBar = () => {
+  const router = useRouter();
+  const [searchAddress, setSearchAddress] = useState(null);
+
+  const searchUserNft = (e) => {
+    e.preventDefault();
+    if (!searchAddress) {
+      alert("Enter valid address");
+    } else {
+      console.log(searchAddress);
+      router.push(`/nft-explorer/${searchAddress}`);
+    }
+    //  Todo, refactor into paths
+  };
+
+  return (
+    <div className="mt-10 sm:mt-12">
+      <form className="sm:max-w-xl sm:mx-auto lg:mx-0">
+        <div className="sm:flex">
+          <div className="min-w-0 flex-1">
+            <input
+              id="text"
+              type="text"
+              onInput={e => setSearchAddress(e.target.value)}
+              placeholder="Type address address and search NFTs"
+              className="block w-full px-4 py-3 rounded-md border-2 border-gray-300 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
+            />
+          </div>
+          <div className="mt-3 sm:mt-0 sm:ml-3">
+            <button
+              onClick={searchUserNft}
+              className="block w-full py-3 px-4 rounded-md shadow bg-gray-600 from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
+            >
+              Search NFT
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
--- pages/nft-explorer/index.js
+++ pages/nft-explorer/index.js
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Moralis from 'moralis';
 import { useAccount, useEnsName, useNetwork } from 'wagmi';
-import NftCard from '../../components/Nft';
-import UserInfo from '../../components/Nft/userInfo';
+import SearchBar from '../../components/SearchBar';
 import ArticleCard from '../../components/ArticleCard';
 
 const articles = [
   {
+    guid:'0',
     title: 'Tezos: Web3’s Gritty Underground Art Scene',
     description:
       'Underneath the noise of conspicuous digital consumerism, though, lies a dark, growing...',
@@ -18,6 +18,7 @@ const articles = [
     publishedAt: 'August 25th, 2022',
   },
   {
+    guid:'1',
     title: 'Is NFT Star Daniel Allan Web3’s First Breakout Music...',
     description:
       'Fourteen-hour days on Discord, failures and massive risks: what it takes to be successful in Web3.',
@@ -27,6 +28,7 @@ const articles = [
     publishedAt: 'August 17th, 2022',
   },
   {
+    guid:'2',
     title: 'Music NFTs Breakout Moment?',
     description:
       'Anyone who’s been involved in the web3 space for the past 18 months has heard that music NFTs are going to take over',
@@ -46,14 +48,6 @@ const NftExplorer = () => {
 
   const { chain } = useNetwork();
 
-  const [searchAddress, setSearchAddress] = useState(null);
-
-  const searchUserNft = () => {
-    if (!searchAddress) alert('Enter valid address');
-    //  Todo, refactor into paths
-    router.push(`/nft-explorer/${searchAddress}`);
-  };
-
   const fetchNfts = async () => {
     try {
       await Moralis.start({ apiKey: process.env.NEXT_PUBLIC_MORALIS_API_KEY });
@@ -69,64 +63,24 @@ const NftExplorer = () => {
   };
 
   useEffect(() => {
-    if (!address) return;
     fetchNfts();
   }, []);
 
   return (
     <div>
       <main className='min-h-full flex-col  items-center justify-center  mt-20 max-w-3xl mx-auto sm:px-6 lg:px-8'>
-        <div className='mt-10 sm:mt-12'>
-          <form action='#' className='sm:max-w-xl sm:mx-auto lg:mx-0'>
-            <div className='sm:flex'>
-              <div className='min-w-0 flex-1'>
-                <input
-                  id='text'
-                  type='text'
-                  onChange={(e) => {
-                    setSearchAddress(e.target.value);
-                  }}
-                  placeholder='Type address address and search NFTs'
-                  className='block w-full px-4 py-3 rounded-md border-2 border-gray-300 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900'
-                />
-              </div>
-              <div className='mt-3 sm:mt-0 sm:ml-3'>
-                <button
-                  onClick={searchUserNft}
-                  className='block w-full py-3 px-4 rounded-md shadow bg-gray-600 from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900'
-                >
-                  Search NFT
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-
-        {nftBalances.length ? (
+        <SearchBar />
+        {articles.length ? (
           <>
-            <UserInfo address={address} />
-
-            <div className={'flex flex-row space-x-5 mt-10 sm:mt-12'}>
-              <p className={'text-3xl'}>NFTs</p>
-              <div className={'bg-gray-100 rounded -mt-1'}>
-                <p className={'text-md p-2'}>{chain.name}</p>
-              </div>
-            </div>
-
-            <div className='mt-10 sm:mt-12 grid grid-cols-4 gap-4'>
-              {nftBalances.map((nft, index) => {
-                return <NftCard nft={nft} key={index} />;
-              })}
+            <div className=''>
+              {articles.map((article) => (
+                <ArticleCard article={article} key={article.id}/>
+              ))}
             </div>
           </>
         ) : (
           <div>
             <h1 className={'p-20 text-center'}>No NFTs available</h1>
-            <div className=''>
-              {articles.map((article) => (
-                <ArticleCard article={article} />
-              ))}
-            </div>
           </div>
         )}
       </main>
