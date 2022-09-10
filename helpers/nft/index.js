98759d11d95fb0c083bbc81015b5c86bff6c9639
= <Emmanuelsumeh@gmail.com>
2022-09-10 16:46:30 +0100

    ft: implement rss feed nft standard

--- helpers/nft/index.js
+++ helpers/nft/index.js
@@ -1,11 +1,10 @@
 import {Web3Storage} from 'web3.storage/dist/bundle.esm.min.js'
 
 // function to upload a file to IPFS via web3.storage
-export const uploadFileToWebStorage = async (e) => {
+export const uploadFileToWebStorage = async (file) => {
     // Construct with token and endpoint
-    const client = new Web3Storage({token: process.env.REACT_APP_STORAGE_API_KEY})
-
-    const file = e.target.files;
+    const client = new Web3Storage({token: process.env.NEXT_PUBLIC_STORAGE_API_KEY})
+    console.log({file})
     if (!file) return;
     // Pack files into a CAR and send to web3.storage
     const rootCid = await client.put(file) // Promise<CIDString>
@@ -14,19 +13,22 @@ export const uploadFileToWebStorage = async (e) => {
     const res = await client.get(rootCid) // Promise<Web3Response | null>
     const files = await res.files() // Promise<Web3File[]>
 
-    return `https://ipfs.infura.io/ipfs/${files[0].cid}`;
+    return {
+        imageUrl : `https://infura-ipfs.io/ipfs/${files[0].cid}`,
+        imageSize : files[0].size,
+        imageType : files[0].type
+    };
 }
 
 
 // function to upload a content to IPFS via web3.storage
 export const uploadContentToWeb3Storage = async (data) => {
     // Construct with token and endpoint
-    const client = new Web3Storage({token: process.env.REACT_APP_STORAGE_API_KEY})
+    const client = new Web3Storage({token: process.env.NEXT_PUBLIC_STORAGE_API_KEY})
 
-    
+
     // Pack files into a CAR and send to web3.storage
     const rootCid = await client.put(data) // Promise<CIDString>
-
     // Fetch and verify files from web3.storage
     const res = await client.get(rootCid) // Promise<Web3Response | null>
     const files = await res.files() // Promise<Web3File[]>
--- pages/create-nft/index.js
+++ pages/create-nft/index.js
@@ -4,29 +4,46 @@ import {uploadFileToWebStorage, uploadContentToWeb3Storage} from "../../helpers/
 import {useContractSend} from "../../hooks/contract/useContractWrite";
 
 const MintNft = () => {
-    const [ipfsImage, setIpfsImage] = useState("");
+    const [ipfsImage, setIpfsImage] = useState({
+        imageUrl: null, imageSize: null, imageType: null
+    });
     const [nftName, setNftName] = useState("");
     const [nftDescription, setNftDescription] = useState("");
+    const [nftContent, setNftContent] = useState("");
     const [nftCategory, setNftCategory] = useState("");
     const {address} = useAccount()
-    const {chain} =  useNetwork()
+    const { chain } = useNetwork();
+
 
     // check if all form data has been filled
     const isFormFilled = () =>
-        nftName && ipfsImage && nftDescription
+        nftName && ipfsImage.imageUrl && nftDescription
 
-    const createNft = async () => {
-            if (!nftName || !nftDescription || !ipfsImage ||!nftCategory) return;
-            // convert NFT metadata to JSON format
-            const data = JSON.stringify({
-                name : nftName ,
-                description : nftDescription,
-                image: ipfsImage,
-                category: nftCategory,
-                owner: address,
-            });
+    const createNft = async (e) => {
+        e.preventDefault()
+        const {imageUrl, imageSize, imageType} = ipfsImage
+        if (!nftName || !nftDescription || !ipfsImage.imageUrl || !nftCategory || !nftContent) return;
 
-            try {
+        // convert NFT metadata to JSON format
+        const data = JSON.stringify({
+            name: nftName,
+            description: nftDescription,
+            image: imageUrl,
+            'nms-article': [
+                {title: nftName},
+                {link: nftContent},
+                {description: nftDescription},
+                {author: address},
+                {category: nftCategory},
+                {"enclosure-url": imageUrl, length: imageSize, type:imageType},
+                {pubDate: new Date()},
+                {blockchain: chain.name},
+                {"guid": "xyz"},
+                {"editions": "1"},
+                {"edition": "1"}
+            ]
+        });
+        try {
                 const ipfsUrl = uploadContentToWeb3Storage(data)
                 console.log({ipfsUrl})
                 const args = [address, ipfsUrl]
@@ -64,23 +81,89 @@ const MintNft = () => {
 
                         <div>
                             <label htmlFor="email" className="block text-lg font-medium text-gray-700">
-                                Write article content
+                                Short Description
                             </label>
                             <div className="mt-1">
-                                <textarea
+                                <input
                                     id="text"
-                                    name="text"
+                                    name="name"
+                                    type="text"
                                     onChange={(e) => {
                                         setNftDescription(e.target.value)
                                     }}
-                                    rows={5}
+                                    autoComplete="name"
                                     required
                                     className="appearance-none block w-full px-3 py-2 border border-2 border-black rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                 />
                             </div>
                         </div>
 
+                        {/*<div>*/}
+                        {/*    <label htmlFor="email" className="block text-lg font-medium text-gray-700">*/}
+                        {/*        Write article content*/}
+                        {/*    </label>*/}
+                        {/*    <div className="mt-1">*/}
+                        {/*        <textarea*/}
+                        {/*            id="text"*/}
+                        {/*            name="text"*/}
+                        {/*            onChange={(e) => {*/}
+                        {/*                setNftContent(e.target.value)*/}
+                        {/*            }}*/}
+                        {/*            rows={5}*/}
+                        {/*            required*/}
+                        {/*            className="appearance-none block w-full px-3 py-2 border border-2 border-black rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"*/}
+                        {/*        />*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
 
+                        <div className="mt-6">
+                            <label className="block text-sm font-medium text-gray-700">
+                                Upload article markdown
+                            </label>
+
+                            <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                                <div className="space-y-1 text-center">
+                                    <svg
+                                        className="mx-auto h-12 w-12 text-gray-400"
+                                        stroke="currentColor"
+                                        fill="none"
+                                        viewBox="0 0 48 48"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        />
+                                    </svg>
+                                    <div className="flex text-sm text-gray-600">
+                                        <label
+                                            htmlFor="file-upload"
+                                            className="relative cursor-pointer bg-white rounded-md font-medium text-green-600 hover:text-green-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-green-500"
+                                        >
+
+                                            <input
+                                                id="file-upload"
+                                                name="file-upload"
+                                                type="file"
+                                                onChange={ async (e)=> {
+                                                    const {
+                                                        imageUrl : contentUrl
+                                                    } = await uploadFileToWebStorage(e.target.files)
+                                                    if (!contentUrl) {
+                                                        alert("failed to upload image");
+                                                        return;
+                                                    }
+                                                    setNftContent(contentUrl)
+                                                }}
+                                            />
+                                        </label>
+                                        {/*<p className="pl-1">attach your file here</p>*/}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
                         <div>
                             <label htmlFor="email" className="block text-lg font-medium text-gray-700">
@@ -101,13 +184,14 @@ const MintNft = () => {
                             </div>
                         </div>
 
+
                         <div className="mt-6">
                             <label className="block text-sm font-medium text-gray-700">
                                 Cover photo
                             </label>
-                            {ipfsImage && (
-                                <div>
-                                    <img src={ipfsImage} alt="cover" />
+                            {ipfsImage?.imageUrl && (
+                                <div className={'flex flex-row justify-center'}>
+                                    <img src={ipfsImage.imageUrl} alt="cover"/>
                                 </div>
                             )}
                             <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
@@ -136,18 +220,23 @@ const MintNft = () => {
                                                 id="file-upload"
                                                 name="file-upload"
                                                 type="file"
-                                                className="sr-only"
-                                                onChange={ async (e)=>{
-                                                    const imageUrl = await uploadFileToWebStorage(e)
+                                                onChange={ async (e)=> {
+                                                    const {
+                                                        imageUrl,
+                                                        imageSize,
+                                                        imageType
+                                                    } = await uploadFileToWebStorage(e.target.files)
                                                     if (!imageUrl) {
                                                         alert("failed to upload image");
                                                         return;
                                                     }
-                                                    setIpfsImage(imageUrl);
+                                                    setIpfsImage({
+                                                        imageUrl, imageSize, imageType
+                                                    });
                                                 }}
                                             />
                                         </label>
-                                        <p className="pl-1">attach your file here</p>
+                                        {/*<p className="pl-1">attach your file here</p>*/}
                                     </div>
                                 </div>
                             </div>
