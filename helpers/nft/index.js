cf15d3cd8ea5b403b98c6fb829ad41bf16253a9d
= <Emmanuelsumeh@gmail.com>
2022-09-14 12:03:36 +0100

    fix: log error

--- helpers/nft/index.js
+++ helpers/nft/index.js
@@ -2,20 +2,26 @@ import {Web3Storage, File, Blob} from 'web3.storage/dist/bundle.esm.min.js'
 import axios from "axios";
 // function to upload a file to IPFS via web3.storage
 export const uploadFileToWebStorage = async (file) => {
-    // Construct with token and endpoint
-    const client = new Web3Storage({token: process.env.NEXT_PUBLIC_STORAGE_API_KEY})
-    if (!file) return;
-    // Pack files into a CAR and send to web3.storage
-    const rootCid = await client.put(file) // Promise<CIDString>
-    // Fetch and verify files from web3.storage
-    const res = await client.get(rootCid) // Promise<Web3Response | null>
-    const files = await res.files() // Promise<Web3File[]>
+    try {
+        // Construct with token and endpoint
+        const client = new Web3Storage({token: process.env.NEXT_PUBLIC_STORAGE_API_KEY})
+        console.log('stuff ', process.env.NEXT_PUBLIC_STORAGE_API_KEY  )
+        if (!file) return;
+        // Pack files into a CAR and send to web3.storage
+        const rootCid = await client.put(file) // Promise<CIDString>
+        // Fetch and verify files from web3.storage
+        const res = await client.get(rootCid) // Promise<Web3Response | null>
+        const files = await res.files() // Promise<Web3File[]>
 
-    return {
-        imageUrl : `https://infura-ipfs.io/ipfs/${files[0].cid}`,
-        imageSize : files[0].size,
-        imageType : files[0].type
-    };
+        return {
+            imageUrl : `https://infura-ipfs.io/ipfs/${files[0].cid}`,
+            imageSize : files[0].size,
+            imageType : files[0].type
+        };
+    }catch (e) {
+        console.log({e})
+    }
+
 }
 
 
